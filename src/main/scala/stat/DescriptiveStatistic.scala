eb7912e544c9071839ef83c3163455ef54679b7e
unknown <marcelosantana@MARCELOSANTANA-NTB.POSITIVO.CORP>
2014-08-29 16:31:32 -0300

    Test for descriptive statistic

--- src/main/scala/stat/DescriptiveStatistic.scala
+++ src/main/scala/stat/DescriptiveStatistic.scala
@@ -9,12 +9,17 @@ object DescriptiveStatistic {
 
   def variance(inputs: Array[Double]): Double = {
     val mean: Double = DescriptiveStatistic.mean(inputs)
-    return inputs.map(x => math.pow(x - mean, 2)).foldLeft(0.0)((acum: Double, x: Double) => acum + x) / inputs.length
+    inputs.map(x => math.pow(x - mean, 2)).foldLeft(0.0)((acum: Double, x: Double) => acum + x) / inputs.length
+  }
+
+  def meanVariance(inputs: Array[Double]): (Double, Double) = {
+    val mean: Double = DescriptiveStatistic.mean(inputs)
+    (mean, inputs.map(x => math.pow(x - mean, 2)).foldLeft(0.0)((acum: Double, x: Double) => acum + x) / inputs.length)
   }
 
   def standardDeviation(inputs: Array[Double]): Double = {
     val variance = DescriptiveStatistic.variance(inputs)
-    return math.sqrt(variance)
+    math.sqrt(variance)
   }
 
   def minMax(inputs: Array[Double]): (Double, Double) = {
@@ -24,24 +29,21 @@ object DescriptiveStatistic {
       if (x < min) min = x
       if (x > max) max = x
     }
-    return (min, max)
+    (min, max)
   }
 
-  def varianceAndStandardDeviation(inputs: Array[Double]): (Double, Double) = {
-    val variance = DescriptiveStatistic.variance(inputs)
+  def meanVarianceAndStandardDeviation(inputs: Array[Double]): (Double, Double, Double) = {
+    val (mean, variance) = DescriptiveStatistic.meanVariance(inputs)
     val standardDeviation = math.sqrt(variance)
-    // returns a tuple with the variance and standard deviation
-    return (variance, standardDeviation)
+    (mean, variance, standardDeviation)
   }
 
-  /*def calculateDescriptiveStatistics(input: Array[Double]): DescriptiveStatData = {
-    var min, max, mean, variance, standardDeviation = 0.0
-    mean = DescriptiveStatistic.mean(input)
-    (min, max) = minMax(input)
-    (variance, standardDeviation) = varianceAndStandardDeviation(input)
-    return new DescriptiveStatData(mean, variance, standardDeviation, min, max)
-  }*/
-
+  def calculateDescriptiveStatistics(input: Array[Double]): DescriptiveStatData = {
+    require(input != null && input.length > 0)
+    val (min, max) = minMax(input)
+    val (mean, variance, standardDeviation) = meanVarianceAndStandardDeviation(input)
+    new DescriptiveStatData(mean, variance, standardDeviation, min, max)
+  }
 }
 
 class DescriptiveStatData(val mean: Double, val variance: Double, val standardDeviation: Double, val min: Double, val max: Double)
--- src/test/scala/TestStatTools.scala
+++ src/test/scala/TestStatTools.scala
@@ -1,8 +1,26 @@
 import org.scalatest.FlatSpec
+import stat.DescriptiveStatistic
 
 /**
  * Created by marcelosantana on 25/08/2014.
  */
 class TestStatTools extends FlatSpec {
+
+  "An descriptive statistic" should "calculate " in {
+    val input = Array(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0)
+    val descriptiveStatData =
+      DescriptiveStatistic.calculateDescriptiveStatistics(input)
+    assert(descriptiveStatData.min == 0.0)
+    assert(descriptiveStatData.max == 10.0)
+    assert(descriptiveStatData.mean == 5.0)
+    assert(descriptiveStatData.variance == 10.0)
+    assert(descriptiveStatData.standardDeviation > 3.16 && descriptiveStatData.standardDeviation < 3.17)
+  }
+
+  it should "produce throw IllegalArgumentException when input is null or empty" in {
+    intercept[IllegalArgumentException] {
+      DescriptiveStatistic.calculateDescriptiveStatistics(Array[Double]())
+    }
+  }
 
 }
